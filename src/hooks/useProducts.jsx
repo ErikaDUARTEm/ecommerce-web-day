e98022f0a738cb0076cd225fbc57720a88504c4d
Angelica Garcia <anyegar36@gmail.com>
2023-05-17 14:25:16 -0500

    changes

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,9 +1,12 @@
 import { useContext } from 'react'
 import { ProductsContext } from '../context/Products'
+
 export function useProducts () {
   const products = useContext(ProductsContext)
+
   if (!products) {
     throw new Error('useProducts must be used within a ProductsProvider')
   }
+
   return products
 }
