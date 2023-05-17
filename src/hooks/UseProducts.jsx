445d161ca199999b289a646ebc78e408c748ede2
FerRomero1990 <aidafernanda1990@hotmail.com>
2023-05-17 12:54:01 -0500

    use products

---
+++ src/hooks/UseProducts.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import { ProductsContext } from '../context/Products'
+export function useProducts () {
+  const products = useContext(ProductsContext)
+  if (!products) {
+    throw new Error('useProducts must be used within a ProductsProvider')
+  }
+  return products
+}
