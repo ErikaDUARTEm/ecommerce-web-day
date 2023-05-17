72cc57fb82988468f95a0a20dc2d3bff9f1dc113
Erika Duarte <erika.duarte.mora@gmail.com>
2023-05-17 13:24:44 -0500

    change app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 
-import './App.css'
-
+import { Products } from './components/Products'
 function App () {
   return (
     <>
-      <h1>hola mundo</h1>
+      <Products />
     </>
-
   )
 }
-
 export default App
