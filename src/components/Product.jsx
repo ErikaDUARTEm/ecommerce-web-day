55f026bc8f12e6c841a85955676fc094b073b6b2
Angelica Garcia <anyegar36@gmail.com>
2023-05-18 14:34:05 -0500

    change

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -32,6 +32,7 @@ export function Product () {
                     <span className='icon-cart-add' />
                   </button>
                 </div>
+                <p />
               </div>
 
             </article>
